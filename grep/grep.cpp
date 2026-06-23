ad40f9e821ae94244251c2ceb953536e0e3a5148
russpv <russellpeavey@gmail.com>
2026-06-23 19:20:22 +0800

    passes tests
    wip
    
    wip

---
+++ grep/grep.cpp
@@ -0,0 +1,246 @@
+#include <regex>
+#include <iostream>
+#include <string>
+#include <cassert>
+
+#include "Logger.hpp"
+
+struct Args
+{
+	uint8_t opts = 0;
+	static constexpr uint8_t NoCase = 1 << 0; //i
+	static constexpr uint8_t Fixed = 1 << 1; //F
+	static constexpr uint8_t Invert = 1 << 2; //v
+
+	static constexpr uint8_t After = 1 << 3; //A
+	static constexpr uint8_t Before = 1 << 4; //B
+	static constexpr uint8_t Both = 1 << 5; //C
+
+	int aN = 0;
+	int bN = 0;
+	char* pattern = NULL;
+};
+
+bool isnum(unsigned char c) {
+	return (c <= '9' && c >= '0');
+}
+
+int cheap_atoi(char *s, int len) {
+	int num = 0;
+	int i = -1;
+
+	while (++i < len) {
+		num *= 10;
+		num += s[i] - '0';
+	}
+	return num;
+}
+
+int parse_args(int argc, char **argv, Args& o) {
+	enum PendingNum {
+		None,
+		After,
+		Before,
+		Both
+	};
+
+	int seen_pat = 0;
+	PendingNum need_num = None;
+	int num;
+
+	for (int i = 1; i < argc; i++) { // each word
+		if (need_num != None && !isnum(argv[i][0])) {
+			LOG(LOG_ERROR, "Expected context length number: " << argv[i]); 
+			return 1;
+		}	
+		if (need_num != None) {
+			int j = 0;
+			while (argv[i][++j] != '\0') { 
+				char c = argv[i][j];
+				if (!isnum(c)) {LOG(LOG_ERROR, "Invalid num: " << argv[i]); return 1;} 
+			}
+			if (j > 5) {LOG(LOG_ERROR, "Brooo ... size does matter: " << argv[i]); return 1;};
+			num = cheap_atoi(argv[i], j);
+			LOG(LOG_WARN, "Got N: " << num);
+			switch (need_num) {
+				case Both : o.aN = num; o.bN = num; break;
+				case Before : o.bN = num; break;
+				case After : o.aN = num; break;
+				default : break;
+			}
+			need_num = None;
+			continue;
+		}
+		if (argv[i][0] == '-' && !seen_pat) {
+			int j = 1;
+			while (argv[i][j] != '\0') { // nums come as next arg
+				char c = argv[i][j];
+				if (need_num != None) {LOG(LOG_ERROR, "Expected context length number"); return 1;}
+				switch (c) {
+					case 'i' : o.opts |= Args::NoCase; break;
+					case 'F' : o.opts |= Args::Fixed; break;
+					case 'v' : o.opts |= Args::Invert; break;
+					case 'A' : o.opts |= Args::After; need_num = After; break;
+					case 'B' : o.opts |= Args::Before; need_num = Before; break;
+					case 'C' : o.opts |= Args::Both; need_num = Both; break;
+					default: LOG(LOG_ERROR, "Invalid option: " << std::string() + c); return 1;
+				}
+				j++;
+			}
+		} else if (argv[i][0] == '-' && seen_pat) { //check position
+			LOG(LOG_ERROR, "Invalid arg: " << std::string(argv[i]));
+			return 1;
+		} else if (!seen_pat) { // empty pattern ok
+			seen_pat = 1;
+			o.pattern = argv[i];
+			LOG(LOG_WARN, "Got pattern: " << std::string(o.pattern));
+		}
+	}
+	if (0 == seen_pat) {LOG(LOG_ERROR, "No pattern given"); return 1;}
+	return 0;
+}
+
+
+void flush(std::vector<std::string>& ring, int idx, int& count, const Args& o) {
+	int start = idx - count;
+	start = (start < 0) ? start + o.bN : start;
+
+	LOG(LOG_WARN, "flushing (count: " << count << "): " << ring[start]);
+	int i = 0;
+	while (i++ < count) {
+		std::cout << ring[start] << "\n";
+		ring[start].clear();
+		start = (start + 1) % o.bN;
+	}
+	count = 0;
+}
+
+struct MatchRes {
+	bool got_match = false;
+	size_t last = 0;
+};
+
+MatchRes match(const std::string& line, std::stringstream& out, const std::regex& pat, const Args& o) {
+	MatchRes r;
+	int do_find = (o.opts & Args::Fixed);
+
+	if (do_find) {
+		size_t pos = line.find(o.pattern);
+		const std::string pat(o.pattern);
+		while (pos != std::string::npos) {
+			out << line.substr(r.last, pos);
+			out << "\033[31m";
+			out << pat;
+			out << "\033[0m";
+
+			r.last = pos + pat.length();
+			pos = line.find(pat, r.last);
+		}
+	}
+
+	if (!do_find) {
+		// highlight matches
+		auto begin = 	std::sregex_iterator(line.begin(), line.end(), pat);
+		auto end = 	std::sregex_iterator();
+
+
+		for (auto it = begin; it != end; ++it) {
+			auto match = *it;
+
+			out << line.substr(r.last, match.position() - r.last);
+			out << "\033[31m";
+			out << match.str();
+			out << "\033[0m";
+
+			r.last = match.position() + match.length();
+		}
+	}
+	if (r.last > 0) r.got_match = true;
+	return r;
+}
+
+int do_ops(const Args& o) {
+	int invert = (o.opts & Args::Invert);
+
+	std::regex_constants::syntax_option_type flags = std::regex::ECMAScript;
+	if (o.opts & Args::NoCase) flags |= std::regex::icase;
+	std::regex pat(o.pattern, flags);
+
+	std::vector<std::string> ring(o.bN); //calloc
+	int idx=0, count=0;
+
+	int exitc = 1;
+	int dist = 0;
+	int afters = 0;
+	bool out_after = (o.opts & Args::After || o.opts & Args::Both) ? true : false;
+	bool out_before = (o.opts & Args::Before || o.opts & Args::Both) ? true : false;
+
+	LOG(LOG_WARN, "A: " << o.aN << ", " << "B: " << o.bN << ", F: " << 1 *(o.opts & Args::Fixed));
+	for (std::string line; std::getline(std::cin, line);) {
+		std::stringstream out;
+
+		MatchRes r = match(line, out, pat, o); 
+		if (out.tellp() > 0) out << line.substr(r.last);
+
+		if (invert) r.got_match = r.got_match ? false : true;
+
+		// no match, cache it (or print it)
+		if (!r.got_match) {
+			++dist;
+			if (out_after && afters > 0) {
+				if (out.tellp() > 0)
+					std::cout << out.str() << "\n";
+				else
+					std::cout << line << "\n";
+				--afters;
+				continue;
+			}
+			if (out_before) {
+				if (out.tellp() > 0)
+					ring[idx] = std::move(out.str());
+				else
+					ring[idx] = std::move(line);
+				LOG(LOG_WARN, "Cached: " << ring[idx]);
+				idx = (idx + 1 ) % o.bN;
+				count = std::min(count + 1, o.bN);
+			}
+			continue;
+		}
+		exitc = 0;
+		assert(r.got_match && exitc == 0);
+		// got match, print it
+		if (out_after) afters = o.aN; //set/reset counter
+		if (out_before) {
+			if (dist > o.aN + o.bN)
+				std::cout << "\033[36m" << "--" << "\033[0m" << "\n";
+			flush(ring, idx, count, o);
+		}
+		dist = 0;
+		if (out.tellp() > 0)
+			std::cout << out.str() << "\n";
+		else
+			std::cout << line << "\n";
+
+	}
+	return exitc;
+}
+
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		LOG(LOG_WARN, "Usage: input | grep [-iFvABC] pattern");
+		return 1;
+	}
+
+	Args o = {};
+	if (0 != parse_args(argc, argv, o))
+		return 1;
+
+	int res = 0;
+	try {
+		res = do_ops(o);
+	} catch (std::exception& e) {
+		return 1;
+	}
+
+	return res;
+}
--- grep/tester.sh
+++ grep/tester.sh
@@ -25,37 +25,41 @@ check() {
 
 printf "\n------- test cases --------\n"
 n=1
-check "$((n++)). grep hello..." "$(echo "hello" | "$GREP" hello | strip)" "$(echo "hello" | grep hello)"
-check "$((n++)). grep -i hello..." "$(echo "hello" | "$GREP" -i hello | strip)" "$(echo "hello" | grep -i hello)"
-check "$((n++)). grep -v hello..." "$(echo "hello" | "$GREP" -v hello | strip)" "$(echo "hello" | grep -v hello)"
-check "$((n++)). grep -F hello..." "$(echo "hello" | "$GREP" -F hello | strip)" "$(echo "hello" | grep -F hello)"
+check "$((n++)). grep ..." "$(echo "hello" | "$GREP" hello | strip)" "$(echo "hello" | grep hello)"
+check "$((n++)). grep -i ..." "$(echo "hello" | "$GREP" -i hello | strip)" "$(echo "hello" | grep -i hello)"
+check "$((n++)). grep -v ..." "$(echo "hello" | "$GREP" -v hello | strip)" "$(echo "hello" | grep -v hello)"
+check "$((n++)). grep -F ..." "$(echo "hello" | "$GREP" -F hello | strip)" "$(echo "hello" | grep -F hello)"
 
-check "$((n++)). grep -ivF hello..." "$(echo "hello" | "$GREP" -ivF hello | strip)" "$(echo "hello" | grep -ivF hello)"
-check "$((n++)). grep -ivF -A 1 hello..." "$(echo "hello" | "$GREP" -ivF -A 1 hello | strip)" "$(echo "hello" | grep -ivF -A 1 hello)"
-check "$((n++)). grep -ivF -A -B 1 xxx..." "$(printf "nah\nxxx\nmeh" | "$GREP" -ivF -A -B 1 xxx 2>/dev/null | strip 2>/dev/null )" \
-	"$(printf "nah\nxxx\nmeh" | grep -ivF -A -B 1 xxx 2>/dev/null)"
-check "$((n++)). grep -ivF -A 1 -B 1 xxx..." "$(printf "nah\nxxx\nmeh" | "$GREP" -ivF -A 1 -B 1 xxx 2>/dev/null | strip 2>/dev/null )" \
+check "$((n++)). grep -ivF ..." "$(echo "hello" | "$GREP" -ivF hello | strip)" "$(echo "hello" | grep -ivF hello)"
+check "$((n++)). grep -ivF -A 1 ..." "$(echo "hello" | "$GREP" -ivF -A 1 hello | strip)" "$(echo "hello" | grep -ivF -A 1 hello)"
+check "$((n++)). grep -ivF -A -B 1 ..." "$(printf "nah\nxxx\nmeh" | "$GREP" -ivF -A -B 1 xxx 2>/dev/null | strip 2>/dev/null )" \
+	"$(printf "nah\n...eh" | grep -ivF -A -B 1 xxx 2>/dev/null)"
+check "$((n++)). grep -ivF -A 1 -B 1 ..." "$(printf "nah\nxxx\nmeh" | "$GREP" -ivF -A 1 -B 1 xxx 2>/dev/null | strip 2>/dev/null )" \
 	"$(printf "nah\nxxx\nmeh" | grep -ivF -A 1 -B 1 xxx 2>/dev/null)"
-check "$((n++)). grep -ivF -AB 1 xxx..." "$(printf "nah\nxxx\nmeh" | "$GREP" -ivF -AB 1 xxx 2>/dev/null | strip 2>/dev/null)" \
+check "$((n++)). grep -ivF -AB 1 ..." "$(printf "nah\nxxx\nmeh" | "$GREP" -ivF -AB 1 xxx 2>/dev/null | strip 2>/dev/null)" \
 	"$(printf "nah\nxxx\nmeh" | grep -ivF -AB 1 xxx 2>/dev/null)"
-check "$((n++)). grep -C 1 xxx..." "$(printf "nah\nxxx\nmeh" | "$GREP" -ivF -AB 1 xxx 2>/dev/null | strip 2>/dev/null)" \
-	"$(printf "nah\nxxx\nmeh" | grep -ivF -AB 1 xxx 2>/dev/null)"
+check "$((n++)). grep -C 1 ..." "$(printf "nah\nxxx\nmeh" | "$GREP" -ivF -C 1 xxx 2>/dev/null | strip 2>/dev/null)" \
+	"$(printf "nah\nxxx\nmeh" | grep -ivF -C 1 xxx 2>/dev/null)"
 
-check "$((n++)). grep -A 1 -B 2 match..." "$(printf "a\nmatch\nb\nb\nc\nd\nmatch\nc" | "$GREP" -A 1 -B 2 match | strip)" \
+check "$((n++)). grep -A 1 -B 2 ..." "$(printf "a\nmatch\nb\nb\nc\nd\nmatch\nc" | "$GREP" -A 1 -B 2 match | strip)" \
     "$(printf "a\nmatch\nb\nb\nc\nd\nmatch\nc" | grep -A 1 -B 2 match)"
-check "$((n++)). grep -A 1 -B 2 -A 2 match..." "$(printf "a\nmatch\nb\nb\nc\nd\nmatch\nc" | "$GREP" -A 1 -B 2 -A 2 match | strip)" \
+check "$((n++)). grep -A 1 -B 2 -A 2 (non-contig ranges)..." "$(printf "a\nmatch\nb\nb\nc\nd\nmatch\nc" | "$GREP" -A 1 -B 2 -A 2 match | strip)" \
     "$(printf "a\nmatch\nb\nb\nc\nd\nmatch\nc" | grep -A 1 -B 2 -A 2 match)"
 check "$((n++)). grep -B 2 -A 2 (overlap ranges)..." "$(printf "a\nmatch\nb\nmatch\nc" | "$GREP" -A 1 -B 2 -A 2 match | strip)" \
     "$(printf "a\nmatch\nb\nmatch\nc" | grep -A 1 -B 2 -A 2 match)"
 check "$((n++)). grep -B 2 -A 2 (contiguous ranges)..." "$(printf "a\nmatch\nb\nc\nmatch\nz" | "$GREP" -A 1 -B 2 -A 2 match | strip)" \
-    "$(printf "a\nmatch\nb\nmatch\nc" | grep -A 1 -B 2 -A 2 match)"
+    "$(printf "a\nmatch\nb\nc\nmatch\nz"| grep -A 1 -B 2 -A 2 match)"
+
+# arg overwrites
+check "$((n++)). grep -C 2 -A 1 (arg overwrite)..." "$(printf "a\nmatch\nb\nc\nmatch\nz" | "$GREP" -C 2 -A 1 match | strip)" \
+    "$(printf "a\nmatch\nb\nc\nmatch\nz" | grep -C 2 -A 1 match)"
 
 printf "\n------- exit code --------\n"
 
-echo "hello" | "$GREP" foo >/dev/null 2>&1
-check "$((n++)). no match exits 1..." "$?" "1"
+echo "hello..." | "$GREP" foo >/dev/null 2>&1
+check "$((n++)). no match, exits 1..." "$?" "1"
 
-echo "hello" | "$GREP" hello >/dev/null 2>&1
+echo "hello..." | "$GREP" hello >/dev/null 2>&1
 check "$((n++)). match exits 0..." "$?" "0"
 
 printf "\n------- results --------\n"
